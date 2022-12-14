d022af273f398eef968be328cc0898f93f257cc3
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-14 00:03:25 -0800

    add messages for validation

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,21 +6,47 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z
+    .string({
+      required_error: "DATABASE_URL is required",
+      invalid_type_error: "DATABASE_URL must be a string",
+    })
+    .url({ message: "Invalid Database URL" }),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
-      ? z.string().min(1)
+      ? z
+          .string({
+            required_error: "NEXTAUTH_SECRET is required",
+            invalid_type_error: "NEXTAUTH_SECRET must be a string",
+          })
+          .min(1)
       : z.string().min(1).optional(),
   NEXTAUTH_URL: z.preprocess(
     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL
+      ? z.string({
+          required_error: "NEXTAUTH_URL is required",
+          invalid_type_error: "NEXTAUTH_URL must be a string",
+        })
+      : z
+          .string({
+            required_error: "NEXTAUTH_URL is required",
+            invalid_type_error: "NEXTAUTH_URL must be a string",
+          })
+          .url({ message: "Invalid NEXTAUTH_URL" })
   ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_CLIENT_ID: z.string({
+    required_error: "DISCORD_CLIENT_ID is required",
+    invalid_type_error: "DISCORD_CLIENT_ID must be a string",
+  }),
+  DISCORD_CLIENT_SECRET: z.string({
+    required_error: "DISCORD_CLIENT_SECRET is required",
+    invalid_type_error: "DISCORD_CLIENT_SECRET must be a string",
+  }),
 });
 
 /**
