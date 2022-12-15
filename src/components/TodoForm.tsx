f812c285d809a8465cd67b017005d6fa85f9b560
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-15 04:18:09 -0800

    able to add todos to database

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,35 @@
+import { trpc } from "~/utils/trpc";
+import { type MouseEvent, useRef } from "react";
+
 const TodoForm: React.FC = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
+  const addTodo = trpc.todo.add.useMutation();
+
+  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (titleRef?.current?.value) {
+      const input = {
+        title: titleRef?.current?.value,
+        description: descriptionRef?.current?.value,
+      };
+
+      addTodo.mutate(input);
+    }
+  };
+
   return (
     <>
       <form className="flex flex-col">
         <input
+          ref={titleRef}
           placeholder="Title your todo..."
           className="mb-2 rounded-md bg-gray-200 py-1 px-2"
         />
         <textarea
+          ref={descriptionRef}
           placeholder="Describe your todo..."
           className="mb-3 rounded-md bg-gray-200 py-1 px-2"
         />
@@ -17,7 +40,10 @@ const TodoForm: React.FC = () => {
           >
             Reset
           </button>
-          <button className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-blue-500 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <button
+            onClick={handleSubmit}
+            className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-blue-500 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
             Submit
           </button>
         </div>
