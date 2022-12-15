425c1a52bb073960fe4455e603d5fd082b0193f1
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-14 16:55:56 -0800

    added reset to form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -11,7 +11,10 @@ const TodoForm: React.FC = () => {
           className="mb-3 rounded-md bg-gray-200 py-1 px-2"
         />
         <div className="flex flex-row justify-around">
-          <button className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-gray-400 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <button
+            type="reset"
+            className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-gray-400 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
             Reset
           </button>
           <button className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-blue-500 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
