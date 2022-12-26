7779e361daa809e32a54f5e79174101df4de1add
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-25 20:27:42 -0800

    title shrinks if overflowing

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -11,16 +11,16 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
 
   return (
     <>
-      <div className="flex w-72 flex-row items-center rounded-md border-2 border-black px-2 py-2 shadow-md md:w-96">
-        <input
-          type="checkbox"
-          className="border-3 mr-4 h-6 w-6 cursor-pointer rounded-lg checked:bg-green-500"
-        />
-        <div className="w-56 md:w-80">
-          <p className="overflow-hidden text-ellipsis text-xl font-medium">
-            {todo.title}
-          </p>
-          <p className="text-sm font-normal">{relative_completed_by}</p>
+      <div className="flex w-72 flex-row items-center justify-between rounded-md border-2 border-black px-2 py-2 shadow-md md:w-96">
+        <div className="flex w-full flex-row items-center overflow-hidden text-ellipsis">
+          <input
+            type="checkbox"
+            className="border-3 mr-4 h-6 w-6 cursor-pointer rounded-lg checked:bg-green-500"
+          />
+          <div className="shrink">
+            <p className="text-xl font-medium">{todo.title}</p>
+            <p className="text-sm font-normal">{relative_completed_by}</p>
+          </div>
         </div>
         <button className="ml-2 h-6 w-6 rounded-md bg-blue-500 p-1 text-white hover:bg-blue-700">
           <PencilIcon className="h-4 w-4" aria-hidden="true" />
