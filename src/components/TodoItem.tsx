b3d08d3ea836ed42a1d93373172e2684d3eb269b
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-25 19:38:08 -0800

    changed edit dropdown to button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,6 @@
 import { type Todo } from "@prisma/client";
-import {
-  EllipsisHorizontalIcon,
-  PencilIcon,
-} from "@heroicons/react/24/outline";
+import { PencilIcon } from "@heroicons/react/24/outline";
 import { formatRelative } from "date-fns";
-import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
 
 interface TodoItemProps {
   todo: Todo;
@@ -16,7 +11,7 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-between rounded-md border-2 border-black px-4 py-2 align-middle shadow-md">
+      <div className="flex w-full flex-row items-center justify-between rounded-md border-2 border-black px-2 py-2 shadow-md">
         <input
           type="checkbox"
           className="border-3 h-6 w-6 cursor-pointer rounded-lg checked:bg-green-500"
@@ -25,39 +20,9 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
           <p className="text-xl font-medium">{todo.title}</p>
           <p className="text-sm font-normal">{relative_completed_by}</p>
         </div>
-        <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <Menu.Button>
-              <EllipsisHorizontalIcon className="h-5 w-5" />
-            </Menu.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 w-fit origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="px-1 py-1 ">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-blue-500 text-white" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                      <PencilIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                      Edit
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+        <button className="h-6 w-6 rounded-md bg-blue-500 p-1 text-white hover:bg-blue-700">
+          <PencilIcon className="h-4 w-4" aria-hidden="true" />
+        </button>
       </div>
     </>
   );
