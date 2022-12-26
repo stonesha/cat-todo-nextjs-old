421aabea8ef7d155ea675e2f26bdbcfcd69aa34a
Stone Sha <36431006+stonesha@users.noreply.github.com>
2022-12-25 20:01:51 -0800

    fixed width and title truncation

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,4 @@
 import { type Todo } from "@prisma/client";
-import { PencilIcon } from "@heroicons/react/24/outline";
 import { formatRelative } from "date-fns";
 
 interface TodoItemProps {
@@ -11,13 +10,15 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
 
   return (
     <>
-      <div className="flex w-full flex-row items-center justify-between rounded-md border-2 border-black px-2 py-2 shadow-md">
+      <div className="flex w-72 flex-row items-center rounded-md border-2 border-black px-2 py-2 shadow-md md:w-96">
         <input
           type="checkbox"
-          className="border-3 h-6 w-6 cursor-pointer rounded-lg checked:bg-green-500"
+          className="border-3 mr-4 h-6 w-6 cursor-pointer rounded-lg checked:bg-green-500"
         />
-        <div>
-          <p className="text-xl font-medium">{todo.title}</p>
+        <div className="w-56 md:w-80">
+          <p className="overflow-hidden text-ellipsis text-xl font-medium">
+            {todo.title}
+          </p>
           <p className="text-sm font-normal">{relative_completed_by}</p>
         </div>
       </div>
