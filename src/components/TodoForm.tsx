077e157e04c7cce5832715d5712f96db72ebb091
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-05 00:04:46 -0800

    form uses react-hook-form and zod

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,16 +1,32 @@
 import { trpc } from "~/utils/trpc";
-import { type MouseEvent, useRef, useEffect } from "react";
 import useStore from "~/utils/useStore";
 import { type Todo } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import format from "date-fns/format";
+import * as z from "zod";
 
 interface TodoFormProps {
   todo?: Todo;
 }
 
+const schema = z.object({
+  title: z.string().min(1, { message: "Required" }),
+  date: z.string().optional(),
+  time: z.string().optional(),
+});
+
+type TodoFormSchemaType = z.infer<typeof schema>;
+
 const TodoForm: React.FC<TodoFormProps> = ({ todo }) => {
-  const titleRef = useRef<HTMLInputElement>(null);
-  const dateRef = useRef<HTMLInputElement>(null);
-  const timeRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    reset,
+  } = useForm<TodoFormSchemaType>({
+    resolver: zodResolver(schema),
+  });
 
   const setEditModalIsOpen = useStore((state) => state.setEditModalIsOpen);
   const editModalIsOpen = useStore((state) => state.editModalIsOpen);
@@ -22,51 +38,47 @@ const TodoForm: React.FC<TodoFormProps> = ({ todo }) => {
     },
   });
 
-  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  if (todo) {
+    const defaultValues: TodoFormSchemaType = {
+      title: todo.title,
+      date: format(todo.complete_by, "yyyy-MM-dd"),
+      time: format(todo.complete_by, "HH:mm"),
+    };
+    reset({ ...defaultValues });
+  }
 
-    if (
-      titleRef?.current?.value &&
-      dateRef?.current?.value &&
-      timeRef?.current?.value
-    ) {
-      const input = {
-        title: titleRef?.current?.value,
-        complete_by: new Date(
-          `${dateRef?.current?.value} ${timeRef?.current?.value}`
-        ),
-      };
+  const onSubmit = async (data: TodoFormSchemaType) => {
+    const input = {
+      title: data.title,
+      complete_by: new Date(`${data.date} ${data.time}`),
+    };
 
-      addTodo.mutate(input);
-      titleRef.current.value = "";
-    }
-
+    addTodo.mutate(input);
     if (editModalIsOpen) setEditModalIsOpen(false);
   };
 
   return (
     <>
-      <form className="mb-2 flex flex-col">
+      <form className="mb-2 flex flex-col" onSubmit={handleSubmit(onSubmit)}>
         <input
-          ref={titleRef}
+          {...register("title")}
           type="text"
           maxLength={255}
           placeholder="Title your todo..."
           className="mb-2 rounded-md bg-gray-200 py-1 px-2"
-          value={todo?.title}
         />
         <div className="flex flex-row justify-around">
           <input
-            ref={dateRef}
+            {...register("date")}
             type="date"
             className="mb-2 rounded-md bg-gray-200 py-1 px-2"
-            value={todo?.complete_by.toISOString().split("T")[0]}
+            disabled={isSubmitting}
           />
           <input
-            ref={timeRef}
+            {...register("time")}
             type="time"
             className="mb-2 rounded-md bg-gray-200 py-1 px-2"
-            value={todo?.complete_by.toISOString().split("T")[1]?.split(".")[0]}
+            disabled={isSubmitting}
           />
         </div>
         <div className="flex flex-row justify-around">
@@ -77,7 +89,7 @@ const TodoForm: React.FC<TodoFormProps> = ({ todo }) => {
             Reset
           </button>
           <button
-            onClick={handleSubmit}
+            type="submit"
             className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-blue-500 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
           >
             Submit
