31791a59725de46b86affa6e6a14b44c80616420
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-05 02:23:58 -0800

    hide reset button if editing a todo

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -83,12 +83,16 @@ const TodoForm: React.FC<TodoFormProps> = ({ todo }) => {
           />
         </div>
         <div className="flex flex-row justify-around">
-          <button
-            type="reset"
-            className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-gray-400 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-          >
-            Reset
-          </button>
+          {todo ? (
+            <></>
+          ) : (
+            <button
+              type="reset"
+              className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-gray-400 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+            >
+              Reset
+            </button>
+          )}
           <button
             type="submit"
             className="flex flex-row items-center justify-center rounded-md border-2 border-white bg-blue-500 px-4 py-2 align-middle text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
